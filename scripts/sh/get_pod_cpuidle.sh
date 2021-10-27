c2ebf0101366beade1a5c811fde7b380b0f7ce15
theGloves <yusfko@hotmail.com>
2021-10-27 23:17:04 +0800

    修改vmstat用法

--- scripts/sh/get_pod_cpuidle.sh
+++ scripts/sh/get_pod_cpuidle.sh
@@ -1,14 +1,13 @@
 # !/usr/bin/env sh
 set -e
-pods=$(kubectl get pods -n tendermint -o name | grep node)
+pods=$(kubectl get pods -n tendermint -o name | grep node0)
 
 gres=""
 
 function get_CPU_IDLE {
-    gres=""
     for p in $pods; do
-        idle=$(kubectl exec ${p} -n tendermint -- "vmstat | tail -n +3 | head -1 " | awk '{print $15}')
-        gres=$gres$idle","
+        idle=`kubectl exec ${p} -n tendermint -- vmstat 1 6 | tail -n +4 | awk '{sum+=$15} END {print sum/NR'} `
+        gres=$idle
     done
 }
 
@@ -16,5 +15,5 @@ function get_CPU_IDLE {
 while [ 1 ] ;do
     get_CPU_IDLE
     echo $gres
-    sleep 5
+    # sleep 5
 done
